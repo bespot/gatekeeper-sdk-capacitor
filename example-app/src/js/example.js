57b659f8b7f06b9b2a246b723aa84477e9dd4bbf
Spyrou Vangelis <spirou.vagelis@gmail.com>
2025-11-27 14:19:42 +0200

    - Created the UI for example app with 'Check Now', 'Subscribe', 'Unsubscribe' and 'Ask for permissions` buttons.

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,6 +1,120 @@
 import { SafeSDK } from 'gatekeeper-sdk-capacitor';
 
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    SafeSDK.echo({ value: inputValue })
-}
+window.customElements.define(
+  'capacitor-welcome',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      const root = this.attachShadow({ mode: 'open' });
+
+      root.innerHTML = `
+    <style>
+      :host {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
+      h1, h2, h3, h4, h5 {
+        text-transform: uppercase;
+      }
+      .buttons-row {
+        display: flex;
+        gap: 10px;
+        width: 100%;
+      }
+      .buttons-row .button {
+        flex: 1;
+        padding: 10px 16px;
+      }
+      .button {
+        display: inline-block;
+        padding: 10px;
+        background-color: #73B5F6;
+        color: #fff;
+        font-size: 0.9em;
+        border: 0;
+        border-radius: 8px;
+        text-decoration: none;
+        cursor: pointer;
+      }
+      main {
+        padding: 15px;
+      }
+      main hr { height: 1px; background-color: #eee; border: 0; }
+      main h1 {
+        font-size: 1.4em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+      }
+      main h2 {
+        font-size: 1.1em;
+      }
+      main h3 {
+        font-size: 0.9em;
+      }
+      main p {
+        color: #333;
+      }
+      main pre {
+        white-space: pre-line;
+      }
+    </style>
+    <div>
+      <capacitor-welcome-titlebar>
+        <h1>Capacitor</h1>
+      </capacitor-welcome-titlebar>
+      <main>
+        <p>
+          <button class="button" id="Check Now" style="background-color: #007BFF; color: white; display: block; width: 100%">Check Now</button>
+        </p>
+        <div class="buttons-row">
+          <button class="button" id="Subscribe" style="background-color: #28A745; color: white;">Subscribe</button>
+          <button class="button" id="Unsubscribe" style="background-color: #FA120A; color: white;">Unsubscribe</button>
+        </div>
+        <p>
+          <button class="button" id="Ask for permissions" style="background-color: #1E3F4A; color: white; display: block; width: 100%">Ask for permissions</button>
+        </p>
+        <p>
+          <img id="image" style="max-width: 100%">
+        </p>
+      </main>
+    </div>
+    `;
+    }
+
+    connectedCallback() {
+      const self = this;
+    }
+  },
+);
+
+window.customElements.define(
+  'capacitor-welcome-titlebar',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const root = this.attachShadow({ mode: 'open' });
+      root.innerHTML = `
+    <style>
+      :host {
+        position: relative;
+        display: block;
+        padding: 15px 15px 15px 15px;
+        text-align: center;
+        background-color: #ffffff;
+      }
+      ::slotted(h1) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-size: 0.9em;
+        font-weight: 600;
+        color: #fff;
+      }
+    </style>
+    <slot></slot>
+    `;
+    }
+  },
+);
